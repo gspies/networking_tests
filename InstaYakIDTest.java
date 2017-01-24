255c42afa30e46e98d629647628182ca8ce38bcf
gspies <gstotx@hotmail.com>
2017-01-23 21:04:25 -0600

    Update InstaYakIDTest.java

--- InstaYakIDTest.java
+++ InstaYakIDTest.java
@@ -1,3 +1,12 @@
+/************************************************
+*
+* Author: Greg Spies
+* Assignment: Program 0
+* Class: Data Communications
+* Testing partner: Daniel Holt
+*
+************************************************/
+
 package instayak.serialization.test;
 
 import static org.junit.Assert.assertArrayEquals;
@@ -11,6 +20,7 @@ import java.io.UnsupportedEncodingException;
 
 import org.junit.Test;
 
+import instayak.serialization.InstaYakChallenge;
 import instayak.serialization.InstaYakException;
 import instayak.serialization.InstaYakID;
 import instayak.serialization.InstaYakMessage;
@@ -19,7 +29,7 @@ import instayak.serialization.MessageOutput;
 
 public class InstaYakIDTest {
 	
-	private static final byte[] encoding;
+	private static byte[] encoding;
 	private static String ID;
 	
 	
@@ -34,10 +44,7 @@ public class InstaYakIDTest {
 		}
 	}*/
 	
-	@BeforeClass
-	public static void setup(){
-	   
-	}
+	
 	
 	@Test
 	public void testEncode() throws IOException, InstaYakException {
@@ -50,11 +57,47 @@ public class InstaYakIDTest {
 
 	@Test
 	public void testDecodeMessageInput() throws InstaYakException, IOException {
-	    
-	    MessageInput min = new MessageInput(new ByteArrayInputStream(encoding));
-		InstaYakID idMsg = (InstaYakID) InstaYakMessage.decode(min);
+	    encoding = "ID bob\r\n".getBytes("ISO8859-1");
+  	    MessageInput min = new MessageInput(new ByteArrayInputStream(encoding));
+  		InstaYakID idMsg = (InstaYakID) InstaYakMessage.decode(min);
 		assertEquals("bob", idMsg.getID());
 	}
+	
+	@Test
+    public void testDecodeMessageTripleInput() 
+            throws InstaYakException, IOException {
+        encoding = "ID bob\r\nID bob\r\nID bob\r\n".getBytes("ISO8859-1");
+        MessageInput min = new MessageInput(new ByteArrayInputStream(encoding));
+        InstaYakID idMsg = (InstaYakID) InstaYakMessage.decode(min);
+        assertEquals("bob", idMsg.getID());
+        InstaYakID idMsg2 = (InstaYakID) InstaYakMessage.decode(min);
+        assertEquals("bob", idMsg2.getID());
+        InstaYakID idMsg3 = (InstaYakID) InstaYakMessage.decode(min);
+        assertEquals("bob", idMsg3.getID());
+    }
+	
+	@Test (expected = NullPointerException.class)
+    public void testDecodeMessageNullInput() 
+            throws InstaYakException, IOException {
+        encoding = "".getBytes("ISO8859-1");
+        MessageInput min = new MessageInput(new ByteArrayInputStream(encoding));
+        InstaYakID idMsg = (InstaYakID) InstaYakMessage.decode(min);
+        assertEquals("bob", idMsg.getID());
+    }
+	
+	@Test(expected = InstaYakException.class)
+    public void testDecodeMessageBadInput() 
+            throws InstaYakException, IOException {
+        encoding = "ID 56bob\r65\n46dg\r\n".getBytes("ISO8859-1");
+        MessageInput min = new MessageInput(new ByteArrayInputStream(encoding));
+        InstaYakID idMsg = (InstaYakID) InstaYakMessage.decode(min);
+    }
+	
+	/*
+	 * 
+	 * IOEXCEPTION TEST
+	 * 
+	 */
 	
 	@Test (expected = InstaYakException.class)
 	public void testSetIDBadInput1() throws InstaYakException{
@@ -71,5 +114,12 @@ public class InstaYakIDTest {
     public void testEncodeBadID() throws InstaYakException{
         InstaYakID id = new InstaYakID("hello");
         assertEquals("hello", id.getID());
+    }
+	
+	@Test
+    public void testIDEquals() throws InstaYakException {
+       InstaYakID id1 = new InstaYakID("abc97");
+       InstaYakID id2 = new InstaYakID("abc97");
+       assertTrue(id1.equals(id2));
     }
 }
